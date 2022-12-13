290c22f98f5504fc819eef54e0478d46f8ec525d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 04:06:20 +0000

    Adding tarfile member sanitization to extractall()

--- worker/worker.py
+++ worker/worker.py
@@ -467,7 +467,26 @@ def restore_server(
     logger.error(f"Extracting backup to canonical location at {host_path}")
     os.chdir(host_path)
     with tarfile.open(backup_location, "r:gz") as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
 
     # Start the server.
     start_container(docker_client, server, host_path)
